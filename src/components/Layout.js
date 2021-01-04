9a98e1d1e7f37d85c2849affb10b78aa033d16d2
unknown <assafliron1@gmail.com>
2021-01-04 19:09:15 +0200

    rules added

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -11,13 +11,13 @@ const boardStyle = {
     height: '350px',
     margin: '5px',
     display: 'grid',
-    gridTemplate: 'repeat(7, 1fr) / repeat(7, 1fr)'
+    gridTemplate: 'repeat(7, 1fr) / repeat(7, 1fr)',
 };
 
 const resetStyle = {
     borderWidth:'2px',
-    width:'55px',
-    height:'25px',
+    width:'100px',
+    height:'30px',
     borderRadius: '20px',
     outline: 'none',
     background: '#7FFFD4',
@@ -25,7 +25,8 @@ const resetStyle = {
 
 const pStyle = {
     fontFamily: 'courier',
-    fontSize: '160%',  
+    fontSize: '160%',
+    textAlign: 'center'
 };
 
 const Layout = ({boardTiles, tileOnClick, winner, resetOnClick}) => {
@@ -34,11 +35,19 @@ const Layout = ({boardTiles, tileOnClick, winner, resetOnClick}) => {
     ));
     return(
         <>
+        <h1 style={{textAlign: 'center'}}>Four In A Row </h1>
+        <h3 style={{textAlign: 'center'}}>The Rules:</h3>
+        <p  style={{textAlign: 'center'}}>
+            It is the goal of the game to connect four of your tokens in a line.
+            All directions (vertical, horizontal, diagonal) are allowed.
+            Players take turns putting one of their tokens into one of the seven slots.
+            A token falls down as far as possible within a slot.
+        </p>
         <div style={boardStyle}>
             {renderedTiles}
         </div>
         <div >
-            <button onClick={resetOnClick} style={resetStyle}>Reset</button>
+            <button onClick={resetOnClick} style={resetStyle}>New Game</button>
             <p style={pStyle}>{winner ? `The winner is: ${winner===PLAYER_ONE ? 'Red Player' : 'Blue Player'}`: ''}</p>
         </div>
         </>
